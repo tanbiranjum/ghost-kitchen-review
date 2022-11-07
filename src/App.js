a08fb85a6f88284eff216b3bc0f167697d8b424d
tanbiranjum <tanbiranjum@hotmail.com>
2022-11-08 00:55:31 +0600

    react routed added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./router/Router/router";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/router/Router/router.js
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../layout/Main";
+import Home from "../../pages/Home/Home";
+import Login from "../../pages/Login/Login";
+import Register from "../../pages/Register/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
+
+export default router;
