c4fae13d9fe87e21fc8dc1ac2c90d9c05f062cb5
tanbiranjum <tanbiranjum@hotmail.com>
2022-11-08 13:26:49 +0600

    course data loading functionality in router

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
 import useDocumentTitle from "../../hooks/useDocumentTitle";
 
 const Home = () => {
+  const kitchens = useLoaderData();
   useDocumentTitle("Home - Ghost Kitchen Review", true);
-  return <div>Home</div>;
+  return (
+    <div>
+      {!kitchens && <div>Loading...</div>}
+      {kitchens &&
+        kitchens?.data.map((kitchen) => (
+          <div key={kitchen._id}>
+            <h1>{kitchen.name}</h1>
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default Home;
--- src/router/Router/router.js
+++ src/router/Router/router.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main";
 import Home from "../../pages/Home/Home";
+import Kitchen from "../../pages/Kitchen/Kitchen";
 import Login from "../../pages/Login/Login";
 import Register from "../../pages/Register/Register";
 
@@ -12,6 +13,9 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+        loader: async () => {
+          return fetch("http://localhost:5000/api/v1/kitchens?limit=3");
+        },
       },
       {
         path: "/login",
@@ -20,6 +24,10 @@ const router = createBrowserRouter([
       {
         path: "/register",
         element: <Register />,
+      },
+      {
+        path: "/kitchen",
+        element: <Kitchen />,
       },
     ],
   },
