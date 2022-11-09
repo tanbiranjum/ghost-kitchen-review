072bf8b3f288f245055d3033830128345dbe885e
tanbiranjum <tanbiranjum@hotmail.com>
2022-11-09 14:48:03 +0600

    navbar ui updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,65 +15,64 @@ const Header = () => {
   };
   // TODO - change header (use flowbite header)
   return (
-    <header className="p-4 dark:bg-gray-800 dark:text-gray-100">
-      <div className="container flex justify-between h-16 mx-auto">
-        <div className="flex justify-between w-full">
-          <Link
-            to="/"
-            aria-label="Back to homepage"
-            className="flex justify-center items-center p-2 gap-2"
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
           >
-            <img src={Logo} alt="logo" className="w-10 h-10" />
-            <p className="text-lg text-yellow-500">Ghost Kitchen</p>
-          </Link>
-          <ul className="items-stretch hidden space-x-3 lg:flex">
-            <li className="flex">
+            <li>
               <NavLink
-                rel="noopener noreferrer"
                 to="/"
                 className={({ isActive }) => {
-                  return isActive
-                    ? "flex items-center px-4 -mb-1 border-b-2 dark:border-violet-400"
-                    : "flex items-center px-4 -mb-1 border-b-2 dark:border-transparent";
+                  return isActive ? "text-white" : "";
                 }}
               >
                 Home
               </NavLink>
             </li>
-            <li className="flex">
+            <li>
               <NavLink
-                rel="noopener noreferrer"
                 to="/kitchen"
                 className={({ isActive }) => {
-                  return isActive
-                    ? "flex items-center px-4 -mb-1 border-b-2 dark:border-violet-400"
-                    : "flex items-center px-4 -mb-1 border-b-2 dark:border-transparent";
+                  return isActive ? "text-white" : "";
                 }}
               >
-                Kitchens
+                Kitchen
               </NavLink>
             </li>
-            <li className="flex">
+            <li>
               <NavLink
-                rel="noopener noreferrer"
                 to="/my-reviews"
                 className={({ isActive }) => {
-                  return isActive
-                    ? "flex items-center px-4 -mb-1 border-b-2 dark:border-violet-400"
-                    : "flex items-center px-4 -mb-1 border-b-2 dark:border-transparent";
+                  return isActive ? "text-white" : "";
                 }}
               >
                 My Reviews
               </NavLink>
             </li>
-            <li className="flex">
+            <li>
               <NavLink
-                rel="noopener noreferrer"
                 to="/add-kitchen"
                 className={({ isActive }) => {
-                  return isActive
-                    ? "flex items-center px-4 -mb-1 border-b-2 dark:border-violet-400"
-                    : "flex items-center px-4 -mb-1 border-b-2 dark:border-transparent";
+                  return isActive ? "text-white" : "";
                 }}
               >
                 Add Kitchen
@@ -81,39 +80,72 @@ const Header = () => {
             </li>
           </ul>
         </div>
-        <div className="items-center flex-shrink-0 hidden lg:flex">
-          {!user?.uid && (
-            <button className="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">
-              Log in
-            </button>
-          )}
-          {user?.uid && (
-            <button
-              className="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900"
-              onClick={handleLogout}
-            >
-              Log Out
-            </button>
-          )}
-        </div>
-        <button className="p-4 lg:hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="w-6 h-6 dark:text-gray-100"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
-          </svg>
-        </button>
+        <Link
+          to="/"
+          aria-label="Back to homepage"
+          className="flex justify-center items-center p-2 gap-2"
+        >
+          <img src={Logo} alt="logo" className="w-10 h-10" />
+          <p className="text-xl text-yellow-500 font-bold">Ghost Kitchen</p>
+        </Link>
       </div>
-    </header>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal p-0">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => {
+                return isActive ? "text-white" : "";
+              }}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/kitchen"
+              className={({ isActive }) => {
+                return isActive ? "text-white" : "";
+              }}
+            >
+              Kitchen
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/my-reviews"
+              className={({ isActive }) => {
+                return isActive ? "text-white" : "";
+              }}
+            >
+              My Reviews
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/add-kitchen"
+              className={({ isActive }) => {
+                return isActive ? "text-white" : "";
+              }}
+            >
+              Add Kitchen
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        {!user?.uid && (
+          <Link to="/login" className="btn">
+            Log in
+          </Link>
+        )}
+        {user?.uid && (
+          <button className="btn" onClick={handleLogout}>
+            Log Out
+          </button>
+        )}
+      </div>
+    </div>
   );
 };
 
